2415f6d83d1555dd43ed7f2f121b3e1d75cdc8f8
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-02-15 10:02:02 +0100

    working clint-server with multiple clients

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,6 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static java.lang.Integer.parseInt;
+
 public class ClientHandler implements Runnable{
+    private Socket forbindelse;
+    private InputStreamReader leseforbindelse;
+    private PrintWriter skriveren;
+    private BufferedReader leseren;
+
+    public ClientHandler(Socket clientSocket) throws IOException {
+        this.forbindelse = clientSocket;
+        leseforbindelse = new InputStreamReader(forbindelse.getInputStream());
+        leseren = new BufferedReader(leseforbindelse);
+        skriveren = new PrintWriter(forbindelse.getOutputStream(), true);
+    }
+
     @Override
     public void run() {
+        try {
+            skriveren.println("Hey, you have contact with the server!");
+            skriveren.println("In this program you can choose to either add or subtract two numbers.");
+            skriveren.println("You can quit this program by pressing the Enter button.");
+            skriveren.println("Enter a number:");
+
+            String enLinje = leseren.readLine();
+            while (enLinje != null) {
+                System.out.println("\nFirst number entered: " + enLinje);
+                skriveren.println("Next");
+                enLinje = leseren.readLine();
+                System.out.println("Second number entered: " + enLinje);
+                skriveren.println("Next");
+                enLinje = leseren.readLine();
+                System.out.println("Addition(a) or subtraction(s): " + enLinje);
+
+                if(enLinje.equals("a")){
+                    enLinje = leseren.readLine();
+                    System.out.println("The answer is: " +enLinje);
+                }
+                else if(enLinje.equals("s")){
+                    enLinje = leseren.readLine();
+                    System.out.println("The answer is: "+enLinje);
+                }
+                else System.out.println("The client has entered something wrong.");
+                enLinje = leseren.readLine();
+            }
+        }catch (IOException e){
+            System.err.println("IO exception in client handler.");
+            System.err.println(e.getStackTrace());
+        }
+        finally {
+            try {
+                leseren.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            skriveren.close();
+            try {
+                forbindelse.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
--- src/SocketTjener.java
+++ src/SocketTjener.java
@@ -1,47 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class SocketTjener {
+  private static ArrayList<ClientHandler> clients = new ArrayList<>();
+  private static ExecutorService pool = Executors.newFixedThreadPool(10);
+
   public static void main(String[] args) throws IOException {
     final int PORTNR = 1250;
-
     ServerSocket tjener = new ServerSocket(PORTNR);
-    System.out.println("Log for the serverside. We are waiting...");
-    Socket forbindelse = tjener.accept();
 
-    InputStreamReader leseforbindelse = new InputStreamReader(forbindelse.getInputStream());
-    BufferedReader leseren = new BufferedReader(leseforbindelse);
-    PrintWriter skriveren = new PrintWriter(forbindelse.getOutputStream(), true);
-
-    skriveren.println("Hey, you have contact with the server!");
-    skriveren.println("In this program you can choose to either add or subtract two numbers.");
-    skriveren.println("You can quit this program by pressing the Enter button.");
-    skriveren.println("Enter a number:");
-
-    String enLinje = leseren.readLine();
-    while (enLinje != null) {
-      System.out.println("\nFirst number entered: " + enLinje);
-      skriveren.println("Next");
-      enLinje = leseren.readLine();
-      System.out.println("Second number entered: " + enLinje);
-      skriveren.println("Next");
-      enLinje = leseren.readLine();
-      System.out.println("Addition(a) or subtraction(s): " + enLinje);
-
-      if(enLinje.equals("a")){
-        enLinje = leseren.readLine();
-        System.out.println("The answer is: " +enLinje);
-      }
-      else if(enLinje.equals("s")){
-        enLinje = leseren.readLine();
-        System.out.println("The answer is: "+enLinje);
-      }
-      else System.out.println("The client has entered something wrong.");
-      enLinje = leseren.readLine();
+    while (true) {
+      System.out.println("Log for the serverside. We are waiting...");
+      Socket forbindelse = tjener.accept();
+      ClientHandler clientThread = new ClientHandler(forbindelse);
+      clients.add(clientThread);
+      pool.execute(clientThread);
     }
-
-    leseren.close();
-    skriveren.close();
-    forbindelse.close();
   }
 }
